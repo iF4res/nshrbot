940818d8bd50ef567a5d54ba4c993ed0acd69e2f
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-18 22:41:54 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+client.user.setGame(`دنيا صعبه ادري اني`, "https://twitch.tv/Humm")
+client.user.setStatus('dnd')
+})
+
+
+client.on('guildMemberAdd', member => {
+    member.sendMessage(`Welcome \n بوتنا العربي الجديد بخواص خورافيه \n ادخل وجرب بنفسك \n https://discord.gg/ \n الدعوه خاصه لك [${member}]`);
+});
+
+
+client.on('guildMemberAdd', member => {
+    member.sendMessage(`GoodBye \n بوتنا العربي الجديد بخواص خورافيه \n ادخل وجرب بنفسك \n https://discord.gg/ \n الدعوه خاصه لك [${member}]`);
+});
